21990326c95505024bee3082f4dabc1b301cc3d2
Emran Khan <khanemranpau@gmail.com>
2023-11-28 12:45:50 +0600

    New Files uploaded
    Using localStorage, JSON.stringify, JSON.parse, manifest file.

--- index.js
+++ index.js
@@ -1,50 +1,60 @@
-
 let myLeads = []
-const inputEl = document.getElementById('input-el')
-const btnEl = document.getElementById('btn-el')
-const ulEl = document.getElementById('ul-el')
-const clearEl = document.getElementById('clear-el')
+const inputEL = document.getElementById('input-el')
+const inputBtn = document.getElementById('input-btn')
+const ulEL = document.getElementById('ul-el')
+const leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+const deleteBtnEl = document.getElementById('delete-btn')
 const tabBtn = document.getElementById('tab-btn')
+console.log(leadsFromLocalStorage)
 
-let leadsFromLocalStorage = JSON.parse(localStorage.getItem('myLeads'))
+
+
 
 tabBtn.addEventListener('click', function () {
-    chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem('myLeads', JSON.stringify(myLeads))
         render(myLeads)
 
     })
+    
 })
 
-btnEl.addEventListener('click', function () {
-    myLeads.push(inputEl.value)
-    localStorage.setItem('myLeads', JSON.stringify(myLeads))
-    render(myLeads)
-    clearInputField()
-})
 
-if (leadsFromLocalStorage) {
+
+
+
+if(leadsFromLocalStorage) {
     myLeads = leadsFromLocalStorage
     render(myLeads)
 }
 
+inputBtn.addEventListener('click', function() {
+    myLeads.push(inputEL.value)
+    inputEL.value = ""
+    localStorage.setItem('myLeads', JSON.stringify(myLeads))
+    render(myLeads)
+})
+
 function render(leads) {
     let listItems = ''
     for (let i = 0; i < leads.length; i++) {
-        listItems += `<li><a target='_blank' href="${leads[i]}">${leads[i]}</a></li>`
+        listItems += 
+        `
+            <a target = '_blank' href= '${leads[i]}'>
+                <li>${leads[i]}</li>
+            </a>
+        `
+        
     }
-    ulEl.innerHTML = listItems
 
+    ulEL.innerHTML = listItems
 }
 
-function clearInputField() {
-    inputEl.value = ''
-
-}
-
-clearEl.addEventListener("dblclick", function () {
-    localStorage.clear(myLeads)
+deleteBtnEl.addEventListener('dblclick', function() {
+    localStorage.clear()
     myLeads = []
     render(myLeads)
 })
+
